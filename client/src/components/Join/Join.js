215072e558787c9f9ba1dc58b607adaf4ca56e0f
aysegulaktas <aysegulaktaass@gmail.com>
2020-06-12 13:27:41 +0300

    join kısmı düzenlendi

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,13 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import './Join.css';
 
 const Join = () => {
-
-    return ( <
-        h1 >
-        JOIN <
-        /h1>
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    return (
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join</h1>
+                <div><input placeholder="Kullanıcı Adı" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
+                <div><input placeholder="Oda" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
+                <Link onClick={event => (!name || !room)? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className="button mt-20" type="submit">Giriş Yap</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('servis çalışıyor');
+    res.send('service is up and running');
 });
 
 module.exports = router;
