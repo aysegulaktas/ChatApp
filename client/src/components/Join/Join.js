0a51c075676c9911ef4cb95a4060024f47221fd7
aysegulaktas <aysegulaktaass@gmail.com>
2020-06-12 13:21:35 +0300

    chat ve join kısımları yayına verildi

---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+import './Join.css';
+
+const Join = () => {
+
+    return ( <
+        h1 >
+        JOIN <
+        /h1>
+    )
+}
+
+export default Join;
